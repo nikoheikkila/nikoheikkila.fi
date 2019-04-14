8123c8a3b7458828f4ace51d0781c751145a2d79
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-04-14 18:16:48 +0300

    fix: update bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,10 +7,12 @@ class Bio extends React.Component {
       <section className='bio'>
         <img src={profilePic} alt="Niko Heikkilä"/>
 
-        <p style={{ maxWidth: 310 }}>
+        <p>
           A blog by{' '}
-          <a href="https://twitter.com/nikoheikkila" target="_blank" rel="noopener noreferrer me">Niko Heikkilä</a>.{' '}
-          Powered by coffee ☕️ and{' '}
+          <a href="https://twitter.com/nikoheikkila" target="_blank" rel="noopener noreferrer me">Niko Heikkilä</a>{'. '}
+          Powered by coffee, {' '}
+          <a href="https://code.visualstudio.com" target="_blank" rel="noopener noreferrer me">VS Code</a>, {' '}
+          and {' '}
           <a href="https://gatsbyjs.org" target="_blank" rel="noopener noreferrer me">Gatsby</a>.
         </p>
 
