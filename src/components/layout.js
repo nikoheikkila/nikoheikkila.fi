f56b3c19de150a0847a307741a4bd6d6636628b2
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-04-14 11:26:43 +0300

    feat: add dark mode and improve UI

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,31 +1,66 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import banner from '../assets/banner.jpg'
-import { rhythm } from '../utils/typography'
+import Toggle from './toggle'
 
 import '../styles/main.scss'
+import sun from '../assets/sun.png'
+import moon from '../assets/moon.png'
 
 class Layout extends React.Component {
+  state = {
+    theme: null,
+  }
+
+  componentDidMount() {
+    this.setState({ theme: window.__theme });
+    window.__onThemeChange = () => {
+      this.setState({ theme: window.__theme });
+    };
+  }
+
   render() {
     const { title, children } = this.props
+    const { theme } = this.state
 
     const header = (
       <section>
         <Link style={{ boxShadow: `none` }} to="/">
           <img src={banner} alt={title} />
         </Link>
+        <Toggle
+          icons={{
+            checked: (
+              <img
+                src={moon}
+                width="16"
+                height="16"
+                alt="checked"
+                style={{ pointerEvents: 'none' }}
+              />
+            ),
+            unchecked: (
+              <img
+                src={sun}
+                width="16"
+                height="16"
+                alt="unchecked"
+                style={{ pointerEvents: 'none' }}
+              />
+            ),
+          }}
+          checked={theme === 'dark'}
+          onChange={e =>
+            window.__setPreferredTheme(
+              e.target.checked ? 'dark' : 'light'
+            )
+          }
+        />
       </section>
     )
 
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
+      <div className="content">
         <header>{header}</header>
         <main>{children}</main>
       </div>
---
+++ src/html.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class HTML extends React.Component {
+    render() {
+
+        const {
+            htmlAttributes,
+            headComponents,
+            bodyAttributes,
+            preBodyComponents,
+            body,
+            postBodyComponents
+        } = this.props
+
+        return (
+            <html {...htmlAttributes} lang="en">
+                <head>
+                    <meta charSet="utf-8" />
+                    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                    />
+                    {headComponents}
+                </head>
+                <body {...bodyAttributes} className="light">
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+              (function() {
+                window.__onThemeChange = function() {};
+                function setTheme(newTheme) {
+                  window.__theme = newTheme;
+                  preferredTheme = newTheme;
+                  document.body.className = newTheme;
+                  window.__onThemeChange(newTheme);
+                }
+                var preferredTheme;
+                try {
+                  preferredTheme = localStorage.getItem('theme');
+                } catch (err) { }
+                window.__setPreferredTheme = function(newTheme) {
+                  setTheme(newTheme);
+                  try {
+                    localStorage.setItem('theme', newTheme);
+                  } catch (err) {}
+                }
+                var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+                darkQuery.addListener(function(e) {
+                  window.__setPreferredTheme(e.matches ? 'dark' : 'light')
+                });
+                setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
+              })();
+            `,
+                        }}
+                    />
+                    {preBodyComponents}
+                    <div
+                        key="body"
+                        id="___gatsby"
+                        dangerouslySetInnerHTML={{ __html: body }}
+                    />
+                    {postBodyComponents}
+                </body>
+            </html>
+        );
+    }
+}
+
+HTML.propTypes = {
+    htmlAttributes: PropTypes.object,
+    headComponents: PropTypes.array,
+    bodyAttributes: PropTypes.object,
+    preBodyComponents: PropTypes.array,
+    body: PropTypes.string,
+    postBodyComponents: PropTypes.array,
+};
