1c97f0e4c877e47402880345fbf715e91c5b6bb4
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-04-15 19:08:05 +0300

    feat(ui): generate footer social links dynamically

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faRss } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+library.add(fab, faRss)
 export default function Footer() {
 
-    const { site: { siteMetadata: {social, rss} } } = useStaticQuery(graphql`
+    const { site: { siteMetadata: { social, rss } } } = useStaticQuery(graphql`
         query {
             site {
                 siteMetadata {
                     social {
-                        twitter
-                        github
+                        name
+                        url
                     }
                     rss
                 }
@@ -20,20 +25,23 @@ export default function Footer() {
     return (
         <footer>
             <div className="feed">
+                <FontAwesomeIcon icon={faRss} />
                 <a href={rss} target="_blank" rel="noopener noreferrer">
                     RSS
                 </a>
             </div>
 
-            <a href={social.twitter} target="_blank" rel="noopener noreferrer">
-                Twitter
-            </a>{' '}
-
-            &bull;{' '}
-
-            <a href={social.github} target="_blank" rel="noopener noreferrer">
-                GitHub
-            </a>
+            {social.map(({name, url}) => (
+                <div key={name}>
+                    <FontAwesomeIcon icon={['fab', name]} />
+                    <a
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer me"
+                    >{name}
+                    </a>
+                </div>
+                ))}
         </footer>
     );
 }
