320ad8f8bd24ce7effb77624c5ff9e61ef3d22b6
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-04-19 13:55:17 +0300

    feat(js): improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,28 @@
 import React from 'react'
-
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+export default class NotFoundPage extends React.Component {
 
-export default NotFoundPage
+  render() {
+    const { location } = this.props
+
+    return (
+      <Layout location={location} title="404 Not Found">
+        <SEO title="404: Not found" />
+        <h1>🤖 You have erred, but it is human</h1>
+        <p>
+          <code>
+            REASON: ERR_ROBOT_UPRISING_FAILED
+          </code>
+        </p>
+        <p>
+          Alas, the content you so humbly requested is nowhere to be found.
+        </p>
+        <p>
+          <a href="/">↪️ Go back to homepage</a>.
+        </p>
+      </Layout>
+    )
+  }
+}
