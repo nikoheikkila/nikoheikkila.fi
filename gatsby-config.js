c41ccacc47753593ffd5693d7e8349871a269ef3
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-04-19 14:03:45 +0300

    fix: replace favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,7 +122,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/gatsby-icon.png`,
+        icon: `src/static/favicon.png`,
       },
     },
     `gatsby-plugin-offline`,
