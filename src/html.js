8ee612bdb0a323916974e1f4e203050e6305c13a
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-04-21 10:59:42 +0300

    refactor(js): move theme switcher to gatsby-browser.js

--- src/html.js
+++ src/html.js
@@ -25,36 +25,6 @@ export default class HTML extends React.Component {
                     {headComponents}
                 </head>
                 <body {...bodyAttributes} className="light">
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-              (function() {
-                window.__onThemeChange = function() {};
-                function setTheme(newTheme) {
-                  window.__theme = newTheme;
-                  preferredTheme = newTheme;
-                  document.body.className = newTheme;
-                  window.__onThemeChange(newTheme);
-                }
-                var preferredTheme;
-                try {
-                  preferredTheme = localStorage.getItem('theme');
-                } catch (err) { }
-                window.__setPreferredTheme = function(newTheme) {
-                  setTheme(newTheme);
-                  try {
-                    localStorage.setItem('theme', newTheme);
-                  } catch (err) {}
-                }
-                var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
-                darkQuery.addListener(function(e) {
-                  window.__setPreferredTheme(e.matches ? 'dark' : 'light')
-                });
-                setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
-              })();
-            `,
-                        }}
-                    />
                     {preBodyComponents}
                     <div
                         key="body"
