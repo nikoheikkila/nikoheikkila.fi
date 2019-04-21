ed9cd27f0c739e0976ee9561b36949e9c8861c75
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-04-21 10:59:42 +0300

    refactor(js): move theme switcher to gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,32 @@
-// File intentionally left blank
+/* eslint no-console: 0 */
+window.__onThemeChange = function () { }
+
+let preferredTheme = null
+
+const setTheme = newTheme => {
+    window.__theme = newTheme
+    preferredTheme = newTheme
+    document.body.className = newTheme
+    window.__onThemeChange(newTheme)
+}
+
+try {
+    preferredTheme = localStorage.getItem('theme')
+} catch (err) {
+    console.error(`Unable to get stored theme. ${err}`)
+}
+
+window.__setPreferredTheme = newTheme => {
+    setTheme(newTheme)
+
+    try {
+        localStorage.setItem('theme', newTheme)
+    } catch (err) {
+        console.error(`Unable to save theme to storage. ${err}`)
+    }
+}
+
+const darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+darkQuery.addListener(e => window.__setPreferredTheme(e.matches ? 'dark' : 'light'))
+setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'))
