8e0f3a7d208a1f4f9a2a78e1aaf9f076dc308431
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-05-12 21:54:29 +0300

    feat(tests): add automated test for XML sitemap

---
+++ cypress/integration/xml.spec.js
@@ -0,0 +1,50 @@
+/// <reference types="Cypress" />
+
+import { isValidXML } from '../support';
+
+context('RSS Feed', () => {
+
+    /**
+     * Before each test load the RSS feed.
+     */
+    beforeEach(() => {
+        cy.visit('http://localhost:8000')
+    })
+
+    /**
+     * Test case
+     *
+     * Requesting RSS feed should yield a response where:
+     * - status = 200
+     * - headers contain correct content type 'text/xml'
+     * - body contains valid RSS data
+     */
+    it('should be a valid XML document', () => {
+        cy.request('/rss.xml').then(response => {
+            expect(response.status).to.equal(200)
+            expect(response.headers['content-type']).to.include('text/xml')
+            expect(isValidXML(response.body)).to.equal(true)
+        })
+    })
+
+})
+
+context('Sitemap', () => {
+
+    /**
+     * Test case
+     *
+     * Requesting XML sitemap should yield a response where:
+     * - status = 200
+     * - headers contain correct content type 'text/xml'
+     * - body contains valid RSS data
+     */
+    it('should be a valid XML document', () => {
+        cy.request('/sitemap.xml').then(response => {
+            expect(response.status).to.equal(200)
+            expect(response.headers['content-type']).to.include('text/xml')
+            expect(isValidXML(response.body)).to.equal(true)
+        })
+    })
+
+})
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,3 +2,13 @@
 import 'gatsby-cypress/commands'
 
 Cypress.on('uncaught:exception', (err, runnable) => false)
+
+/* Helper functions */
+export const isValidXML = body => {
+    const parser = new DOMParser()
+    const errors = parser
+        .parseFromString(body, 'text/xml')
+        .getElementsByTagName('parsererror')
+
+    return errors.length === 0
+}
