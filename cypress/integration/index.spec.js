3d71a75b1f4f4bcf563241a73f63ca7aa35fcead
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-05-12 19:55:07 +0300

    feat(tests): add and improve Cypress tests

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,30 +1,68 @@
 /// <reference types="Cypress" />
 
 context('Homepage', () => {
+
+    /**
+     * Before each test load the main page.
+     */
     beforeEach(() => {
         cy.visit('http://localhost:8000')
     })
 
-    it('contains index of posts', () => {
+    /**
+     * Test Case
+     *
+     * Main page should contain a collection of one or more post links.
+     */
+    it('should contain index of posts', () => {
         cy.get('.post-title').should(p => {
             expect(p).to.have.length.above(0)
         })
     })
 
-    it('toggle changes between themes', () => {
+    /**
+     * Test Case
+     *
+     * Side menu should pop open by clicking the hamburger button on the side
+     * of the screen. The menu should be hidden when the crossed button is
+     * clicked from the menu.
+     */
+    it('should open the side menu on click', () => {
+        cy.get('.bm-burger-button > button').click()
+        cy.get('.site-menu').should('be.visible')
+
+        cy.get('.bm-cross-button > button').click()
+        cy.get('.site-menu').should('not.be.visible')
+    })
+
+    /**
+     * Test Case
+     *
+     * First, we reveal the sidebar by clicking a burger button. Then we click
+     * the theme switcher and assert that theme has changed to 'dark'.
+     * Next, we repeat the above step and finally assert that the theme has
+     * changed to 'light' again.
+     */
+    it('should support switching themes', () => {
         cy.get('.bm-burger-button > button').click()
 
-        cy.get('#theme-switcher').click()
-        cy.get('body').should('not.have.class', 'light')
-        cy.get('body').should('have.class', 'dark')
+        cy.get('.react-toggle-thumb').click().should(() => {
+            expect(localStorage.getItem('theme')).to.deep.equal('dark')
+        })
 
-        cy.get('#theme-switcher').click()
-        cy.get('body').should('not.have.class', 'dark')
-        cy.get('body').should('have.class', 'light')
+        cy.get('body').should(element => {
+            expect(element).to.have.class('dark')
+            expect(element).to.not.have.class('light')
+        })
+
+        cy.get('.react-toggle-thumb').click().should(() => {
+            expect(localStorage.getItem('theme')).to.deep.equal('light')
+        })
+
+        cy.get('body').should(element => {
+            expect(element).to.have.class('light')
+            expect(element).to.not.have.class('dark')
+        })
     })
 
-    it('renders articles', () => {
-        cy.get('.post-title > a').first().click()
-        cy.url().should('match', /\/blog\/[\w-/]+/)
-    })
 })
