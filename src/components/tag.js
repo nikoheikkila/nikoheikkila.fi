45756ceab09e4428c92dbdf7fdb003a75713e7e5
Niko Heikkilä <yo@nikoheikkila.fi>
2019-05-26 19:55:50 +0300

    feat(ui): wrap each post tag inside own component for displaying

---
+++ src/components/tag.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import slugify from '@sindresorhus/slugify'
+
+import { randomColor } from '../utils/colors'
+
+const Tag = ({ title }) => {
+  const innerText = slugify(title, { decamelize: false, separator: ' ' })
+  const style = {
+    display: 'inline-block',
+    fontWeight: 400,
+    backgroundColor: randomColor(),
+    color: '#FFFFFF',
+    padding: '0 4px',
+    margin: '3px'
+  }
+
+  return <span style={style}>#{innerText}</span>
+}
+
+Tag.propTypes = {
+  title: PropTypes.string.isRequired
+}
+
+export default Tag
