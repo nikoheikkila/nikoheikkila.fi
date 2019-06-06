d5e38ddba56c10d3c1bceadd977afb0b1c2a90d7
Niko Heikkilä <yo@nikoheikkila.fi>
2019-06-06 19:53:26 +0300

    feat(js): enhance 404 page
    Provides nice facts about current day via an API.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,27 +1,31 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import RandomFact from '../components/fact'
 
 export default class NotFoundPage extends React.Component {
 
-  render() {
+render() {
     const { location } = this.props
 
     return (
       <Layout location={location} title="404 Not Found">
         <SEO title="404: Not found" />
         <h1>🤖 You have erred, but it is human</h1>
-        <p>
-          <code>
-            REASON: ERR_ROBOT_UPRISING_FAILED
+
+        <div className='gatsby-highlight' data-language='js'>
+          <code className="language-js">
+            REASON: ERR_CONTENT_NOT_FOUND
           </code>
-        </p>
+        </div>
+
         <p>
           Alas, the content you so humbly requested is nowhere to be found.
+          Why not celebrate this day with the following random fact instead?
         </p>
-        <p>
-          <a href="/">↪️ Go back to homepage</a>.
-        </p>
+
+        <RandomFact />
+
       </Layout>
     )
   }
