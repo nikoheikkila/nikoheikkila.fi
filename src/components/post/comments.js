7f56e6928266dba78e3a11a7ce39b571aabbf103
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-08-02 12:11:36 +0300

    feat(react): observe DOM changes and reload comments

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Disqus } from 'gatsby-plugin-disqus'
@@ -9,22 +10,55 @@ export default class Comments extends Component {
     title: PropTypes.string.isRequired,
   }
 
+  state = {}
+
   constructor(props) {
     super(props)
 
     const { url, identifier, title } = props
 
-    this.config = {
-      url,
-      identifier,
-      title,
+    this.state = {
+      config: {
+        url,
+        identifier,
+        title,
+      },
     }
+
+    /**
+     * Reload Disqus when DOM changes.
+     * TODO: CSS style won't be reapplied on reset and needs to be resolved.
+     */
+    this.observer = new MutationObserver(mutations => {
+      mutations.filter(this.classDidChange).forEach(() =>
+        window.DISQUS.reset({
+          reload: true,
+        })
+      )
+    })
+  }
+
+  componentDidMount() {
+    this.observer.observe(document.body, {
+      attributes: true,
+      childList: false,
+      subtree: false,
+    })
+  }
+
+  componentDidUnMount() {
+    this.observer.disconnect()
+  }
+
+  classDidChange({ type, attributeName }) {
+    return type === 'attributes' && attributeName === 'class'
   }
 
   render() {
+    const { config } = this.state
     return (
       <section id="comments">
-        <Disqus config={this.config} />
+        <Disqus config={config} />
       </section>
     )
   }
