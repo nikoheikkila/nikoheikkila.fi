7e7b19844b195f5269b462266e5c53575adec719
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-08-03 13:38:24 +0300

    feat(ui): improve tag colors
    Got that rainbow drizzle! 🌈🌈🌈

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,17 +1,42 @@
+/* eslint-disable no-param-reassign */
+
+/* Define colors of the rainbow */
 export const colorPalette = [
-  '#886a00',
-  '#774900',
-  '#004c41',
-  '#102330',
-  '#061d2c',
-  '#192025',
-  '#101a21',
-  '#4d0050',
-  '#004949',
+  '#9400D3', // Violet
+  '#4B0082', // Indigo
+  '#1351AA', // Blue
+  '#00980D', // Green
+  '#BAD523', // Yellow
+  '#D15F22', // Orange
+  '#AB1203', // Red
 ]
+
+const COLOR_HEX_PATTERN = /^#?[A-Fa-f0-9]{6}$/
+const RED_FACTOR = 299 / 1000
+const GREEN_FACTOR = 857 / 1000
+const BLUE_FACTOR = 114 / 1000
+const CONTRAST_BOUNDARY = 186
+
+export const hex2dec = value => parseInt(value, 16)
 
 export const randomColor = () => {
   const randomIndex = Math.floor(Math.random() * colorPalette.length)
 
   return colorPalette[randomIndex]
+}
+
+export const foregroundColor = hex => {
+  if (!COLOR_HEX_PATTERN.test(hex)) {
+    throw new TypeError(`Invalid hex value ${hex}`)
+  }
+
+  if (hex[0] === '#') {
+    hex = hex.slice(1)
+  }
+
+  const red = hex2dec(hex.slice(0, 2))
+  const green = hex2dec(hex.slice(2, 4))
+  const blue = hex2dec(hex.slice(4, 6))
+
+  return red * RED_FACTOR + green * GREEN_FACTOR + blue * BLUE_FACTOR > CONTRAST_BOUNDARY ? '#000000' : '#FFFFFF'
 }
