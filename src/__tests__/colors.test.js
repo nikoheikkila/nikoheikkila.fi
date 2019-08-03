3689f4bd29d2ea123bbfca729bde503c844afd64
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-08-03 13:38:24 +0300

    feat(ui): improve tag colors
    Got that rainbow drizzle! 🌈🌈🌈

--- src/__tests__/colors.test.js
+++ src/__tests__/colors.test.js
@@ -1,6 +1,40 @@
 import test from 'ava'
-import { colorPalette, randomColor } from '../utils/colors'
+import { hex2dec, colorPalette, randomColor, foregroundColor } from '../utils/colors'
 
 test('should print a random color', async t => {
+  t.plan(1)
   t.true(colorPalette.includes(randomColor()))
+})
+
+test('should convert hex value to decimal', async t => {
+  t.plan(16)
+  const hexes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+  hexes.forEach((hex, i) => t.is(hex2dec(hex), numbers[i]))
+})
+
+test('should throw error on invalid hex value', async t => {
+  t.plan(2)
+  const error = t.throws(() => foregroundColor('123'), TypeError)
+
+  t.is(error.message, 'Invalid hex value 123')
+})
+
+test('should use light color on dark background', async t => {
+  t.plan(5)
+  const backgrounds = ['#070092', '#295330', '#820009', '#763507', '#00070a']
+
+  backgrounds.forEach(background => {
+    t.is(foregroundColor(background), '#FFFFFF')
+  })
+})
+
+test('should use dark color on light background', async t => {
+  t.plan(5)
+  const backgrounds = ['#f5a7ba', '#b8bfe0', '#a1f98b', '#f4ff93', '#f7f7f7']
+
+  backgrounds.forEach(background => {
+    t.is(foregroundColor(background), '#000000')
+  })
 })
--- src/components/tag.js
+++ src/components/tag.js
@@ -2,17 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import slugify from '@sindresorhus/slugify'
 
-import { randomColor } from '../utils/colors'
+import { randomColor, foregroundColor } from '../utils/colors'
 
 const Tag = ({ title }) => {
   const innerText = slugify(title, { decamelize: false, separator: ' ' })
+  const backgroundColor = randomColor()
+  const color = foregroundColor(backgroundColor)
+
   const style = {
     display: 'inline-block',
-    fontWeight: 400,
-    backgroundColor: randomColor(),
-    color: '#FFFFFF',
-    padding: '0 4px',
-    margin: '3px',
+    fontWeight: 700,
+    backgroundColor,
+    color,
+    padding: '0 6px',
+    margin: '5px',
+    borderRadius: '10%',
   }
 
   return <span style={style}>#{innerText}</span>
