47453335ea08fd166344cb27704d23ae5973ed43
Niko Heikkilä <yo@nikoheikkila.fi>
2019-08-10 11:21:53 +0300

    fix(ui): adjust content image size

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
+              maxWidth: 960,
             },
           },
           {
