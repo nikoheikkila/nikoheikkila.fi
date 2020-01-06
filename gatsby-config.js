9657b48b26d912ad0cc49dc2d1a4d02d0536e838
Niko Heikkilä <yo@nikoheikkila.fi>
2020-01-06 16:18:41 +0200

    feat(md): add support for header auto-linking

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,12 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              icon: false,
+            },
+          },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
