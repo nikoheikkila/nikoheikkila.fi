1c01521210fea0aea1a923966cc7ba66853f2b00
Niko Heikkilä <niko.heikkila@futurice.com>
2022-01-16 14:17:59 +0200

    feat: update to AVA 4 and migrate E2E tests
    E2E tests now use the Playwright Test library as the runner,
    which makes them even smoother.

--- e2e.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    require: ["ts-node/register"],
-    files: ["src/__tests__/feature/*.test.ts"],
-    extensions: ["ts"],
-    concurrency: 5,
-    verbose: true,
-};
---
+++ playwright.config.ts
@@ -0,0 +1,16 @@
+import { PlaywrightTestConfig } from "@playwright/test";
+
+const isPipeline = !!process.env.CI;
+
+const config: PlaywrightTestConfig = {
+    testDir: "src/__tests__/feature",
+    use: {
+        baseURL: process.env.APP_URL || "http://localhost:8000",
+        headless: true,
+        viewport: { width: 1280, height: 720 },
+        ignoreHTTPSErrors: true,
+        video: "retain-on-failure",
+        screenshot: "only-on-failure",
+    },
+};
+export default config;
