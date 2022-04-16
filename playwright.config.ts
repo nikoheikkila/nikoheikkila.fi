67eab4cc549e3ecfe9ada2bc5c217a630af3cc13
Niko Heikkilä <niko.heikkila@futurice.com>
2022-04-16 20:00:01 +0300

    feat(e2e): add more device configurations to tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,45 @@
-import { PlaywrightTestConfig } from "@playwright/test";
+import { PlaywrightTestConfig, devices } from "@playwright/test";
+
+const baseURL = process.env.APP_URL || "http://localhost:8000";
 
 const config: PlaywrightTestConfig = {
     testDir: "src/__tests__/feature",
+    forbidOnly: !!process.env.CI,
     use: {
-        baseURL: process.env.APP_URL || "http://localhost:8000",
-        headless: true,
-        viewport: { width: 1280, height: 720 },
+        baseURL,
         ignoreHTTPSErrors: true,
         video: "retain-on-failure",
         screenshot: "only-on-failure",
+        trace: "retain-on-failure",
     },
+    webServer: {
+        command: "yarn serve",
+        url: baseURL,
+        timeout: 120 * 1000,
+        reuseExistingServer: !process.env.CI,
+    },
+    projects: [
+        {
+            name: "Chromium",
+            use: { ...devices["Desktop Chrome"] },
+        },
+        {
+            name: "Firefox",
+            use: { ...devices["Desktop Firefox"] },
+        },
+        {
+            name: "Safari",
+            use: { ...devices["Desktop Safari"] },
+        },
+        {
+            name: "iPhone",
+            use: { ...devices["iPhone 13"] },
+        },
+        {
+            name: "Android",
+            use: { ...devices["Pixel 5"] },
+        },
+    ],
 };
+
 export default config;
