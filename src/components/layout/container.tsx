d5246f4838ec150f8a5f034ce45a0730173918c1
Niko Heikkilä <niko.heikkila@futurice.com>
2022-12-10 14:32:15 +0200

    fix: TS errors

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -1,13 +1,21 @@
 import React, { FunctionComponent } from "react";
 import * as styles from "./container.module.scss";
 
-export const ListContainer: FunctionComponent = ({ children }) => (
+interface WithChildren {
+    children: React.ReactNode[];
+}
+
+export const ListContainer: FunctionComponent<WithChildren> = ({
+    children,
+}) => (
     <main id="container" className={styles.list}>
         {children}
     </main>
 );
 
-export const SinglePostContainer: FunctionComponent = ({ children }) => (
+export const SinglePostContainer: FunctionComponent<WithChildren> = ({
+    children,
+}) => (
     <main id="container" className={styles.single}>
         {children}
     </main>
