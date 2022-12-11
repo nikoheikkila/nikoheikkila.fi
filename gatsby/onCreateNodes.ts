4b8c45d1105a1ec17f72e3add5ccf671afa9079b
Niko Heikkilä <niko.heikkila@futurice.com>
2022-12-11 20:13:06 +0200

    refactor(gatsby): split gatsby-node.ts to modules

---
+++ gatsby/onCreateNodes.ts
@@ -0,0 +1,58 @@
+import { createFilePath, createRemoteFileNode } from "gatsby-source-filesystem";
+import { CreateNodeArgs, Node } from "gatsby";
+import { MarkdownRemarkFrontmatter } from "../src/types";
+
+interface OnCreateNodeArgs extends CreateNodeArgs {
+    node: Node & {
+        frontmatter: MarkdownRemarkFrontmatter;
+        hero: string | null;
+        [key: string]: unknown;
+    };
+}
+
+const onCreateNodes = async ({
+    node,
+    actions: { createNode, createNodeField },
+    getCache,
+    getNode,
+    createNodeId,
+}: OnCreateNodeArgs) => {
+    if (!isMarkdownNode(node)) {
+        return;
+    }
+
+    const { id, frontmatter } = node;
+    const { hero } = frontmatter;
+
+    if (hero && isRemoteImage(hero)) {
+        const fileNode = await createRemoteFileNode({
+            url: hero,
+            parentNodeId: id,
+            createNode,
+            createNodeId,
+            getCache,
+        });
+
+        if (fileNode) {
+            createNodeField({
+                node,
+                name: "hero",
+                value: fileNode.id,
+            });
+        }
+    }
+
+    const value = createFilePath({ node, getNode });
+
+    createNodeField({
+        name: `slug`,
+        node,
+        value,
+    });
+};
+
+const isMarkdownNode = (node: Node): boolean =>
+    node.internal.type === "MarkdownRemark";
+const isRemoteImage = (url: string): boolean => /^https?:\/\/+/.test(url);
+
+export default onCreateNodes;
---
+++ gatsby/onCreateSchemaCustomization.ts
@@ -0,0 +1,16 @@
+import { CreateSchemaCustomizationArgs } from "gatsby";
+
+const onCreateSchemaCustomization = ({
+    actions: { createTypes },
+}: CreateSchemaCustomizationArgs) =>
+    createTypes(`
+    type SitePage implements Node @dontInfer {
+      path: String!
+    }
+
+    type MarkdownRemark implements Node {
+      hero: File @link(from: "fields.hero")
+    }
+  `);
+
+export default onCreateSchemaCustomization;
