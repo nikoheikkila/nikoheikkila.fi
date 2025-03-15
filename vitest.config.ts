d8ea1deeaaa2c7233c8d0fd830ceabd307ca3a1d
Niko Heikkilä <niko@heikkila.io>
2025-03-15 22:29:55 +0200

    refactor: streamline Vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,18 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vitest/config";
-
-const isPipeline = !!process.env.CI;
 
 export default defineConfig({
 	test: {
 		include: ["src/__tests__/unit/*.test.ts"],
-		reporters: ["verbose"],
-		environment: "node",
-		watch: !isPipeline,
+		coverage: {
+			provider: "v8",
+			reporter: ["text", "lcov"],
+			include: ["src/**/*.ts"],
+		},
+	},
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
 	},
 });
