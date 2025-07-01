5d92ecae6a1b7a7c0472d0bcc912c2b4ffda6c90
Niko Heikkilä <niko@heikkila.io>
2025-07-01 18:21:30 +0300

    refactor(config): sort keys alphabetically

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,14 +5,118 @@ const environment = process.env.NODE_ENV;
 const isProduction = environment === "production";
 
 const config: GatsbyConfig = {
+	flags: {
+		DEV_SSR: false,
+		FAST_DEV: true,
+		PRESERVE_FILE_DOWNLOAD_CACHE: true,
+	},
 	graphqlTypegen: true,
+	plugins: [
+		"gatsby-plugin-image",
+		"gatsby-transformer-sharp",
+		"gatsby-plugin-sharp",
+		{
+			options: {
+				plugins: [
+					{
+						options: {
+							maxWidth: 960,
+						},
+						resolve: "gatsby-remark-images",
+					},
+					{
+						options: {
+							icon: false,
+						},
+						resolve: "gatsby-remark-autolink-headers",
+					},
+					"gatsby-remark-copy-linked-files",
+					"gatsby-remark-smartypants",
+				],
+			},
+			resolve: "gatsby-transformer-remark",
+		},
+		{
+			options: {
+				name: "blog",
+				path: "./content",
+			},
+			resolve: "gatsby-source-filesystem",
+		},
+		{
+			options: {
+				name: "assets",
+				path: "./src/assets",
+			},
+			resolve: "gatsby-source-filesystem",
+		},
+		{
+			options: {
+				feeds: [
+					{
+						match: "^/blog/",
+						output: "/rss.xml",
+						query: rssQuery,
+						serialize,
+						title: "RSS Feed | Niko Heikkilä",
+					},
+				],
+				query: `{
+                    site {
+                        siteMetadata {
+                            title
+                            description
+                            siteUrl
+                            site_url: siteUrl
+                        }
+                    }
+                }`,
+			},
+			resolve: "gatsby-plugin-feed",
+		},
+		{
+			options: {
+				background_color: "#ffffff",
+				crossOrigin: "use-credentials",
+				display: "standalone",
+				icon: "./static/favicon.png",
+				name: "Niko Heikkilä",
+				short_name: "nikoheikkila",
+				start_url: "/",
+				theme_color: "#663399",
+			},
+			resolve: "gatsby-plugin-manifest",
+		},
+		{
+			options: {
+				output: "/robots.txt",
+				policy: [{ allow: "/", userAgent: "*" }],
+			},
+			resolve: "gatsby-plugin-robots-txt",
+		},
+		{
+			options: {
+				sassOptions: {
+					includePaths: ["./src/styles"],
+					silenceDeprecations: ["legacy-js-api"],
+				},
+			},
+			resolve: "gatsby-plugin-sass",
+		},
+		"gatsby-plugin-sitemap",
+		"gatsby-adapter-netlify",
+	].concat(
+		// Load the PWA service worker only in production to enhance development experience.
+		isProduction ? ["gatsby-plugin-offline"] : [],
+	),
 	siteMetadata: {
-		language: "en",
-		title: "Niko Heikkilä",
 		author: {
 			name: "Niko Heikkilä",
 		},
 		description: "A blog by Niko Heikkilä. Powered by coffee, VS Code, and Gatsby.",
+		language: "en",
+		repository: "https://github.com/nikoheikkila/nikoheikkila.fi",
+		rss: "/feed",
 		siteUrl: "https://nikoheikkila.fi",
 		social: [
 			{
@@ -36,113 +140,9 @@ const config: GatsbyConfig = {
 				url: "https://fosstodon.org/@nikoheikkila",
 			},
 		],
-		repository: "https://github.com/nikoheikkila/nikoheikkila.fi",
-		rss: "/feed",
+		title: "Niko Heikkilä",
 	},
 	trailingSlash: "always",
-	flags: {
-		PRESERVE_FILE_DOWNLOAD_CACHE: true,
-		FAST_DEV: true,
-		DEV_SSR: false,
-	},
-	plugins: [
-		"gatsby-plugin-image",
-		"gatsby-transformer-sharp",
-		"gatsby-plugin-sharp",
-		{
-			resolve: "gatsby-transformer-remark",
-			options: {
-				plugins: [
-					{
-						resolve: "gatsby-remark-images",
-						options: {
-							maxWidth: 960,
-						},
-					},
-					{
-						resolve: "gatsby-remark-autolink-headers",
-						options: {
-							icon: false,
-						},
-					},
-					"gatsby-remark-copy-linked-files",
-					"gatsby-remark-smartypants",
-				],
-			},
-		},
-		{
-			resolve: "gatsby-source-filesystem",
-			options: {
-				path: "./content",
-				name: "blog",
-			},
-		},
-		{
-			resolve: "gatsby-source-filesystem",
-			options: {
-				path: "./src/assets",
-				name: "assets",
-			},
-		},
-		{
-			resolve: "gatsby-plugin-feed",
-			options: {
-				query: `{
-                    site {
-                        siteMetadata {
-                            title
-                            description
-                            siteUrl
-                            site_url: siteUrl
-                        }
-                    }
-                }`,
-				feeds: [
-					{
-						serialize,
-						query: rssQuery,
-						output: "/rss.xml",
-						title: "RSS Feed | Niko Heikkilä",
-						match: "^/blog/",
-					},
-				],
-			},
-		},
-		{
-			resolve: "gatsby-plugin-manifest",
-			options: {
-				name: "Niko Heikkilä",
-				short_name: "nikoheikkila",
-				start_url: "/",
-				background_color: "#ffffff",
-				theme_color: "#663399",
-				display: "standalone",
-				icon: "./static/favicon.png",
-				crossOrigin: "use-credentials",
-			},
-		},
-		{
-			resolve: "gatsby-plugin-robots-txt",
-			options: {
-				policy: [{ userAgent: "*", allow: "/" }],
-				output: "/robots.txt",
-			},
-		},
-		{
-			resolve: "gatsby-plugin-sass",
-			options: {
-				sassOptions: {
-					includePaths: ["./src/styles"],
-					silenceDeprecations: ["legacy-js-api"],
-				},
-			},
-		},
-		"gatsby-plugin-sitemap",
-		"gatsby-adapter-netlify",
-	].concat(
-		// Load the PWA service worker only in production to enhance development experience.
-		isProduction ? ["gatsby-plugin-offline"] : [],
-	),
 };
 
 export default config;
