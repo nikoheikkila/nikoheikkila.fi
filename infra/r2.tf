b9dd3d5f4c4bba55d13bff878fe39592de50448b
Niko Heikkilä <niko@heikkila.io>
2025-11-16 19:02:10 +0200

    feat(terraform): add storage class to R2 bucket

--- infra/r2.tf
+++ infra/r2.tf
@@ -1,8 +1,9 @@
 # Cloudflare R2 configuration
 
 resource "cloudflare_r2_bucket" "blog" {
-  account_id   = var.r2_account_id
-  name         = "blog"
-  location     = "EEUR"
-  jurisdiction = "eu"
+  account_id    = var.r2_account_id
+  name          = "blog"
+  location      = "EEUR"
+  storage_class = "Standard"
+  jurisdiction  = "eu"
 }
