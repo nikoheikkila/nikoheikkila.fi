e31620ba005383ccf4cb8ce265dbd70f62d1a84d
Niko Heikkilä <niko@heikkila.io>
2025-11-16 21:38:00 +0200

    fix(terraform): remove redundant quotes in DNS records

--- infra/dns.tf
+++ infra/dns.tf
@@ -5,9 +5,10 @@ locals {
 resource "cloudflare_dns_record" "records" {
   for_each = local.dns_config.dns_records
 
-  zone_id  = var.zone_id
-  name     = each.value.name
-  type     = each.value.type
+  zone_id = var.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  # TXT records must be enclosed in quotes
   content  = each.value.type == "TXT" ? jsonencode(each.value.content) : each.value.content
   ttl      = each.value.ttl
   priority = try(each.value.priority, null)
